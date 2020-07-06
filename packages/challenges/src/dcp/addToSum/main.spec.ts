68f29fac0d6a771b1740834c99b831a25470897d
NiewView <ec.tom.n@gmail.com>
2020-07-06 21:56:12 +0200

    solved first missing positive integer

--- packages/challenges/src/dcp/addToSum/main.spec.ts
+++ packages/challenges/src/dcp/addToSum/main.spec.ts
@@ -1,14 +1,9 @@
-import { doTwoNumbersAddUpToSum } from "./main";
+import { findFirstMissingPositiveInteger } from "./main";
 
-describe("doTwoNumbersAddToSum", () => {
-  test("check with [10, 15, 3, 7]", () => {
-    const inputArr = [10, 15, 3, 7];
-    expect(doTwoNumbersAddUpToSum(inputArr, 15)).toBe(false);
-    expect(doTwoNumbersAddUpToSum(inputArr, 1)).toBe(false);
-    expect(doTwoNumbersAddUpToSum(inputArr, 0)).toBe(false);
-    expect(doTwoNumbersAddUpToSum(inputArr, 12)).toBe(false);
-    expect(doTwoNumbersAddUpToSum(inputArr, 17)).toBe(true);
-    expect(doTwoNumbersAddUpToSum(inputArr, 25)).toBe(true);
-    expect(doTwoNumbersAddUpToSum(inputArr, 10)).toBe(true);
+describe("findFirstMissingPositiveInteger", () => {
+  test("check with [3, 4, -1, 1]", () => {
+    expect(findFirstMissingPositiveInteger([3, 4, -1, 1])).toBe(2);
+    expect(findFirstMissingPositiveInteger([1,2,0])).toBe(3);
+    expect(findFirstMissingPositiveInteger([-1,1,2,0,3,4,-9,10])).toBe(5);
   });
 });
--- packages/challenges/src/dcp/addToSum/main.ts
+++ packages/challenges/src/dcp/addToSum/main.ts
@@ -1,13 +1,12 @@
-export const doTwoNumbersAddUpToSum = (
+export const findFirstMissingPositiveInteger = (
   inputArr: number[],
-  expectedSum: number
-): boolean => {
-  const expectedMap = new Map(inputArr.map((i) => [expectedSum - i, true]));
-  for (let index = 0; index < inputArr.length; index++) {
-    const element = inputArr[index];
-    if (expectedMap.has(element)) {
-      return true;
+): number => {
+  let set = new Set(inputArr);
+  console.log(set)
+  for (let i = 1; i < inputArr.length+1; i++) {
+    if(!set.has(i)){
+      return i;
     }
   }
-  return false;
+  throw new Error("an unkown error occured");
 };
