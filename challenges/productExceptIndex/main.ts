3c7b194a6883695a4fa6779f48220f829a558063
NiewView <ec.tom.n@gmail.com>
2020-08-13 23:15:01 +0200

    removed comments

--- challenges/productExceptIndex/main.ts
+++ challenges/productExceptIndex/main.ts
@@ -6,11 +6,8 @@ export const productExceptIndex = (input: Array<number>) => {
 export const productExceptIndexWithoutDivision = (input: Array<number>) => {
   const product = input
   return input.map((value, index)=>{
-    // console.log(input.splice(index, 1).reduce((accumulator, currentValue) => accumulator * currentValue, 1));
     const copiedInput = [...input];
     copiedInput.splice(index, 1);
-    console.log(copiedInput)
     return copiedInput.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
   })
-  // return input.map(value => product / value);
 }
