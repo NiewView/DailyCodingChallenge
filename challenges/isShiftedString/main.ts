caf4462ee603e54bbf5b94a6430feef1d850e517
Tom <tom.eckardt.it@gmail.com>
2020-10-27 21:38:08 +0100

    added solution for shifted string

--- challenges/isShiftedString/main.ts
+++ challenges/isShiftedString/main.ts
@@ -2,5 +2,16 @@ export const isSecondStringShiftedVariantOfFirst = (
   input: string,
   input2: string
 ): boolean => {
-  return true;
+  if (input === input2) {
+    return true;
+  }
+
+  let shiftedString = input;
+  for (let i = 0; i < input.length; i++) {
+    shiftedString = shiftedString.substr(1) + shiftedString[0];
+    if (shiftedString === input2) {
+      return true;
+    }
+  }
+  return false;
 };
