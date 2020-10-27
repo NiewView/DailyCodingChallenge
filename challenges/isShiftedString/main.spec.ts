24ef1e0a86b74cfd9242b18afaae5e6899b37595
Tom <tom.eckardt.it@gmail.com>
2020-10-27 21:20:35 +0100

    added new challenge and unit tests

---
+++ challenges/isShiftedString/main.spec.ts
@@ -0,0 +1,22 @@
+import { isSecondStringShiftedVariantOfFirst } from "./main";
+
+const inputs = [8, 156, 14, 62, 1568, 352, 4];
+
+describe("isSecondStringShiftedVariantOfFirst", () => {
+  test(`should return true if it is a shifted version`, () => {
+    expect(isSecondStringShiftedVariantOfFirst("abcde", "cdeab")).toBeTruthy();
+    expect(isSecondStringShiftedVariantOfFirst("hallo", "alloh")).toBeTruthy();
+    expect(isSecondStringShiftedVariantOfFirst("hi", "ih")).toBeTruthy();
+    expect(isSecondStringShiftedVariantOfFirst("meter", "meter")).toBeTruthy();
+  });
+
+  test(`should return false if characters are shuffled`, () => {
+    expect(isSecondStringShiftedVariantOfFirst("abc", "acb")).toBeFalsy();
+    expect(isSecondStringShiftedVariantOfFirst("test", "sets")).toBeFalsy();
+  });
+
+  test(`should return if characters do not match at all`, () => {
+    expect(isSecondStringShiftedVariantOfFirst("check", "ceck")).toBeFalsy();
+    expect(isSecondStringShiftedVariantOfFirst("abc", "xyz")).toBeFalsy();
+  });
+});
---
+++ challenges/isShiftedString/main.ts
@@ -0,0 +1,6 @@
+export const isSecondStringShiftedVariantOfFirst = (
+  input: string,
+  input2: string
+): boolean => {
+  return true;
+};
