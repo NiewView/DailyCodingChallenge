b39e19b1ab0966a88d7fbff9f8c167bd2cbde86d
Tom <tom.eckardt.it@gmail.com>
2020-10-31 14:52:02 +0100

    initial not working implementation

--- challenges/longestConsecutiveSequence/main.ts
+++ challenges/longestConsecutiveSequence/main.ts
@@ -1,16 +1,38 @@
 export function longestConsecutiveSequence(input: Array<number>): number {
-  let boundaries = new Map();
+  let maxLength: number = 0; // store maxLength directly to avoid search for maxValue afterwards
+  let boundaries: Map<number, number> = new Map();
   const addValueToBoundary = (newValue: number) => {
     if (boundaries.has(newValue + 1)) {
-      boundaries.get(newValue);
+      const otherBoundary = boundaries.get(newValue + 1)!;
+      const isLowerBound = otherBoundary >= newValue + 1;
+      if (isLowerBound) {
+        boundaries.set(newValue, otherBoundary);
+        boundaries.delete(newValue + 1);
+        boundaries.set(otherBoundary, newValue);
+      }
+      return Math.abs(newValue - otherBoundary);
     }
+    if (boundaries.has(newValue - 1)) {
+      const otherBoundary = boundaries.get(newValue - 1)!;
+      const isUpperBound = otherBoundary <= newValue - 1;
+      if (isUpperBound) {
+        boundaries.set(newValue, otherBoundary);
+        boundaries.delete(newValue - 1);
+        boundaries.set(otherBoundary, newValue);
+      }
+      return Math.abs(newValue - otherBoundary);
+    }
+
+    // add new boundaries
+    boundaries.set(newValue, newValue);
+    return 1;
   };
-  let sequenceLength: Array<number> = [];
+  // Save length as Object, so it is saved by reference
   input.forEach((value) => {
-    if (boundaries.has(value)) {
-      sequenceLength[boundaries.get(value)] += 1;
-    } else {
+    const newDistance = addValueToBoundary(value);
+    if (newDistance > maxLength) {
+      maxLength = newDistance;
     }
   });
-  return 0;
+  return maxLength;
 }
