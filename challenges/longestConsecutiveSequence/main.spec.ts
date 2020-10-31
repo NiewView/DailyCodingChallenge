9cac183a68908925db035cfd67aa04d899b62ad9
Tom <tom.eckardt.it@gmail.com>
2020-10-31 20:57:41 +0100

    solved sequence length

--- challenges/longestConsecutiveSequence/main.spec.ts
+++ challenges/longestConsecutiveSequence/main.spec.ts
@@ -2,12 +2,24 @@ import { longestConsecutiveSequence } from "./main";
 
 describe("staircases", () => {
   test("[100, 4, 200, 1, 3, 2]", () => {
+    // 1-4
     expect(longestConsecutiveSequence([100, 4, 200, 1, 3, 2])).toBe(4);
   });
   test("[2, 20, 4, 3, 10]", () => {
+    // 2-4
     expect(longestConsecutiveSequence([2, 20, 4, 3, 10])).toBe(3);
   });
   test("[-2, -1, 4, 3, 10]", () => {
+    // (-1)-(-2) || 3-4
     expect(longestConsecutiveSequence([-2, -1, 4, 3, 10])).toBe(2);
+  });
+  test("[1,2,-2, -1, 0, 3]", () => {
+    // (-2)-3
+    expect(longestConsecutiveSequence([1, 2, -2, -1, 0, 3])).toBe(6);
+  });
+
+  test("[1,3,5,7,9,-1]", () => {
+    // single number
+    expect(longestConsecutiveSequence([1, 3, 5, 7, 9, -1])).toBe(1);
   });
 });
--- challenges/longestConsecutiveSequence/main.ts
+++ challenges/longestConsecutiveSequence/main.ts
@@ -2,30 +2,50 @@ export function longestConsecutiveSequence(input: Array<number>): number {
   let maxLength: number = 0; // store maxLength directly to avoid search for maxValue afterwards
   let boundaries: Map<number, number> = new Map();
   const addValueToBoundary = (newValue: number) => {
-    if (boundaries.has(newValue + 1)) {
-      const otherBoundary = boundaries.get(newValue + 1)!;
-      const isLowerBound = otherBoundary >= newValue + 1;
-      if (isLowerBound) {
-        boundaries.set(newValue, otherBoundary);
-        boundaries.delete(newValue + 1);
-        boundaries.set(otherBoundary, newValue);
+    const canAppendValueToLowerBound = boundaries.has(newValue + 1);
+    const canAppendValueToUpperBound = boundaries.has(newValue - 1);
+    let boundaryDifference = 0;
+
+    if (canAppendValueToLowerBound && canAppendValueToUpperBound) {
+      const boundaryOne: number = boundaries.get(newValue + 1)!;
+      const boundaryTwo: number = boundaries.get(newValue - 1)!;
+      boundaries.delete(newValue - 1);
+      boundaries.delete(newValue + 1);
+      boundaries.set(boundaryOne, boundaryTwo);
+      boundaries.set(boundaryTwo, boundaryOne);
+      boundaryDifference = boundaryOne - boundaryTwo;
+    } else {
+      if (canAppendValueToLowerBound) {
+        const boundaryOne: number = boundaries.get(newValue + 1)!;
+        const boundaryTwo: number = newValue;
+        const isLowerBound = boundaryOne > boundaryTwo;
+        if (isLowerBound) {
+          boundaries.delete(newValue + 1);
+          boundaries.set(boundaryTwo, boundaryOne);
+          boundaries.set(boundaryOne, boundaryTwo);
+        }
+        boundaryDifference = boundaryOne - boundaryTwo;
       }
-      return Math.abs(newValue - otherBoundary);
-    }
-    if (boundaries.has(newValue - 1)) {
-      const otherBoundary = boundaries.get(newValue - 1)!;
-      const isUpperBound = otherBoundary <= newValue - 1;
-      if (isUpperBound) {
-        boundaries.set(newValue, otherBoundary);
-        boundaries.delete(newValue - 1);
-        boundaries.set(otherBoundary, newValue);
+      if (canAppendValueToUpperBound) {
+        const boundaryOne = boundaries.get(newValue - 1)!;
+        const boundaryTwo: number = newValue;
+        const isUpperBound = boundaryOne <= newValue - 1;
+        if (isUpperBound) {
+          boundaries.delete(newValue - 1);
+          boundaries.set(boundaryTwo, boundaryOne);
+          boundaries.set(boundaryOne, boundaryTwo);
+        }
+        boundaryDifference = boundaryOne - boundaryTwo;
       }
-      return Math.abs(newValue - otherBoundary);
     }
 
-    // add new boundaries
-    boundaries.set(newValue, newValue);
-    return 1;
+    // add value as new boundaries
+    if (!canAppendValueToUpperBound && !canAppendValueToLowerBound) {
+      boundaries.set(newValue, newValue);
+    }
+
+    // add 1, because the amount of numbers is asked not difference
+    return Math.abs(boundaryDifference) + 1;
   };
   // Save length as Object, so it is saved by reference
   input.forEach((value) => {
