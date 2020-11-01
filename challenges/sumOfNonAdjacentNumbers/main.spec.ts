541500626f844a11da327ea3a23d9f9b9aedce8f
Tom <tom.eckardt.it@gmail.com>
2020-11-01 13:16:37 +0100

    solved non adjacent sum

--- challenges/sumOfNonAdjacentNumbers/main.spec.ts
+++ challenges/sumOfNonAdjacentNumbers/main.spec.ts
@@ -1,18 +1,20 @@
+import { nextTick } from "process";
 import { sumOfNonAdjacentNumbers } from "./main";
 
 describe("sumOfNonAdjacentNumbers", () => {
-  test("check for 1 and 2 climbs", () => {
-    expect(getUniqueWaysToClimbStaircase(1)).toBe(1);
-    expect(getUniqueWaysToClimbStaircase(2)).toBe(2);
-    expect(getUniqueWaysToClimbStaircase(3)).toBe(3);
-    expect(getUniqueWaysToClimbStaircase(4)).toBe(5);
-    expect(getUniqueWaysToClimbStaircase(6)).toBe(13);
-    expect(getUniqueWaysToClimbStaircase(18)).toBe(4181);
+  test("[2, 4, 6, 2, 5]", () => {
+    expect(sumOfNonAdjacentNumbers([2, 4, 6, 2, 5])).toBe(13);
   });
-  test("check for 1, 3 and 5 climbs", () => {
-    expect(getUniqueWaysToClimbStaircase(1, [1, 3, 5])).toBe(1);
-    expect(getUniqueWaysToClimbStaircase(2, [1, 3, 5])).toBe(1);
-    expect(getUniqueWaysToClimbStaircase(3, [1, 3, 5])).toBe(2);
-    expect(getUniqueWaysToClimbStaircase(5, [1, 3, 5])).toBe(5);
+  test("[5, 1, 1, 5]", () => {
+    expect(sumOfNonAdjacentNumbers([5, 1, 1, 5])).toBe(10);
+  });
+  test("[3, 7, 2, -2, 1, -3, 6, 1]", () => {
+    expect(sumOfNonAdjacentNumbers([3, 7, 2, -2, 1, -3, 6, 1])).toBe(14);
+  });
+  test("[1, 7, 8, 1, 1, 6, 0, 1, -5]", () => {
+    expect(sumOfNonAdjacentNumbers([1, 7, 8, 1, 1, 6, 0, 1, -5])).toBe(16);
+  });
+  test("[1, 7, 8, 1, 1, 6, 0, 1, -5]", () => {
+    expect(sumOfNonAdjacentNumbers([1, 7, 8, 3, 4, 6, 5, 1, -5])).toBe(18);
   });
 });
--- challenges/sumOfNonAdjacentNumbers/main.ts
+++ challenges/sumOfNonAdjacentNumbers/main.ts
@@ -1,6 +1,21 @@
-export function sumOfNonAdjacentNumbers(
-  n: number,
-  stepsizes: number[] = [1, 2]
-): number {
-  return 0;
+// Honestly I had some real problems solving this challenge.
+// The issue was, that I was focusing on selecting specific numbers, instead of optimizing the overall sum.
+// In my opinion this is a pretty common trap to try replicating how a human solves the problem, instead of optimizing for the goal.
+// I'm trying to avoid this habit, but every now and then it comes through.
+// If you're stuck with a problem try to take a step back and analyze the 'instructions' very carefully and try to focus on the final goal.
+
+export function sumOfNonAdjacentNumbers(input: Array<number>): number {
+  let sumIncluding = 0;
+  let sumExcluding = 0;
+
+  input.forEach((value) => {
+    // One of variables would be enough, second one is only added for better readability
+    let previousSumIncluding = sumIncluding;
+    let previousSumExcluding = sumExcluding;
+
+    sumIncluding = sumExcluding + value;
+    sumExcluding = Math.max(previousSumIncluding, previousSumExcluding);
+  });
+
+  return Math.max(sumIncluding, sumExcluding);
 }
