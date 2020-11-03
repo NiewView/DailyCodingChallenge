72ddc2d2b77412a5568618b31957a4360c353727
Tom <tom.eckardt.it@gmail.com>
2020-11-03 21:14:24 +0100

    solved Number of rooms with time bins

--- challenges/minimumNumberOfRooms/main.ts
+++ challenges/minimumNumberOfRooms/main.ts
@@ -1,35 +1,22 @@
 export function minimumNumberOfRooms(
   schedules: Array<[number, number]>
 ): number {
-  // TODO: improve variable naming
-  // There are only two hard things in Computer Science: cache invalidation and naming things.
-  // -- Phil Karlton
-  if (schedules.length === 0) {
-    return 0;
-  }
+  // generate bins
+  const bins = []
+    .concat(...(schedules as any))
+    .reduce((a, b) => (((a[b] as any) = 0), a), {});
+  // Directly store max number of rooms, so you don't have to search for it afterwards
+  let maxNumber = 0;
 
-  let overlaps: Array<Array<[number, number]>> = [schedules];
-  let schedule = overlaps[0].shift();
-  while (schedule !== undefined) {
-    for (let i = overlaps.length - 1; i >= 0; i--) {
-      const overlap = overlaps[i];
-      overlap.forEach((item) => {
-        if (schedule![0] < item[0] && schedule![1] > item[0]) {
-          if (overlaps.length <= i + 1) {
-            overlaps.push([]);
-          }
-          overlaps[i + 1].push([item[0], schedule![1]]);
-        }
-        if (schedule![0] < item[1] && schedule![1] > item[1]) {
-          if (overlaps.length <= i + 1) {
-            overlaps.push([]);
-          }
-          overlaps[i + 1].push([schedule![0], item[1]]);
-        }
-      });
-    }
-    schedule = overlaps[0].pop();
-  }
-  console.log(JSON.stringify(overlaps));
-  return overlaps.length - 1;
+  schedules.forEach((timeslot) => {
+    Object.keys(bins).forEach((bin) => {
+      if (timeslot[0] <= Number(bin) && timeslot[1] > Number(bin)) {
+        const numberOfRooms = (bins as any)[bin] + 1;
+        maxNumber = maxNumber < numberOfRooms ? numberOfRooms : maxNumber;
+        (bins as any)[bin] = numberOfRooms;
+      }
+    });
+  });
+
+  return maxNumber;
 }
